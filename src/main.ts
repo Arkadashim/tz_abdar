d012d9ee7a5a3475657fa8258c8d053d98dadaac
arkadashiim <arkadashiim@mail.ru>
2025-07-01 21:16:04 +0300

    chore: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common';
 
@@ -8,6 +9,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
   const port = config.get('port') as number;
+
+  const swaggerSetup = new DocumentBuilder().setTitle('TZ_AbDar').build();
+
+  SwaggerModule.setup('docs', app, () =>
+    SwaggerModule.createDocument(app, swaggerSetup),
+  );
 
   app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
