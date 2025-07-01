9b8a5fa4d91a62056ba12807867fd5b08fbe7566
arkadashiim <arkadashiim@mail.ru>
2025-07-01 21:11:47 +0300

    chore: update config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,6 @@
 export default () => ({
   port: parseInt(new String(process.env.PORT).valueOf(), 10) || 3000,
+  evmRpcUrl: process.env.EVM_RPC_URL,
+  cosmosGrpcUrl: process.env.COSMOS_GRPC_URL,
+  cosmosRpcUrl: process.env.COSMOS_RPC_URL,
 });
